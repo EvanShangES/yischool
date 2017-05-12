f337d3677ac456d7cf0728edcedeaac5569c048e
EvanShangES <evanshanges@gmail.com>
2017-05-12 18:32:23 -0400

    Add files via upload

---
+++ app/yixue.app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Evan Shang on 2017-05-10.
+ */
+
+var app = angular.module ($appId, $dependencies);
+
+appConfig(function($stateProvide, $urlRouterProvider){
+
+});
---
+++ app/yixue.config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Evan Shang on 2017-05-10.
+ */
+
+var appConfig = window.appConfig || {};
+
+window.appConfig = appConfig;
+
+var $appId = 'yixue';
+
+var $dependencies = [
+    'ui.router'
+];
+
+var $components = [
+    'yixue.layout',
+    'yixue.home'
+];
